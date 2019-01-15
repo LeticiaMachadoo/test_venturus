8946026ef9ad2d8aae6f604758a180dacbb7c1bb
Letícia Machado <lmachado@orion.tech>
2019-01-15 20:43:36 -0200

    #11: Breadcrumbs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,19 @@ import { ListUsersComponent } from './list-users/list-users.component';
 import { AddUserComponent } from './add-user/add-user.component';
 
 export const ROUTES: Routes = [
-  {path: '', redirectTo: '/users', pathMatch: 'full'},
-  {path: 'users', component: ListUsersComponent},
-  {path: 'users/new', component: AddUserComponent},
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  {
+    path: 'users',
+    component: ListUsersComponent,
+    data: {
+      breadcrumb: 'Listagem de Usuários',
+    }
+  },
+  {
+    path: 'users/new',
+    component: AddUserComponent,
+    data: {
+      breadcrumb: 'Novo Usuário',
+    }
+  }
 ];
